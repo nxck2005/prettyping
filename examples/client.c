5970c1c212c952261573ef0e4122e9bc917cf11f
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-09-15 02:14:24 +0530

    comment out deprecated (?) cmd

--- examples/client.c
+++ examples/client.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     // h_addr err?
-    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    //memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
